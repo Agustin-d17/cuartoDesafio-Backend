d8c321119244a808301c1c7623645dc71390cd47
agustin-d17 <agustindiaz449@gmail.com>
2022-09-08 13:57:00 -0300

    metodos funcionand0

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const { Console } = require('console');
-const fs = require('fs')
-
-
-let newContent = [];
+const fs = require('fs');
 
 class Contenedor {
     constructor(file) {
@@ -20,55 +16,74 @@ class Contenedor {
     }
 
     async save(obj){
-        obj.id = newContent.length + 1
-        newContent.push(obj)
+        const content = await this.readFile();
+        
+            if (content.length !== 0){
+                let newId = content[content.length - 1].id + 1
+                await fs.promises.writeFile(this.file, JSON.stringify([...content, {...obj, id: newId}]), 'utf8');
+                console.log(`id del elemento agregado: ${newId}`)
+            }else{
+                await fs.promises.writeFile(this.file, JSON.stringify([{...obj, id: 1}]), 'utf8');
+                console.log(`id del elemento agregado: ${1}`)
+            }
+
+    }
+
+    async getById(id){
+        const content = await this.readFile()
+
+        const element = content.find(el => el.id === id)
+        // return element
+        console.log(element)
+    }
+
+    async getAll(){
+        const content = await this.readFile()
+        // return content
+        console.log(content) 
+    }
+    
+    async deleteById(id) {
+        try {
+          const content = await this.readFile()
+          newContent = content.filter((product) => product.id !== id)
+          await fs.promises.writeFile(this.file, JSON.stringify(newContent), 'utf-8')
+          console.log(`Se elimino correctamente el elemento de id: ${id}`)
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    
+    async deleteAll(){
+        newContent = []
 
         try {
             await fs.promises.writeFile(this.file, JSON.stringify(newContent), 'utf8') 
-            // console.log(obj.id)
-            return obj.id 
+            console.log("Productos eliminados")
         }catch(err){
             console.log(err)
         }
     }
-
-    // async getById(id){
-    //     const content = await this.readFile()
-
-    //     const element = content.find(el => el.id === id)
-    //     return element
-    // }
-
-    // async getAll(){
-    //     const content = await this.readFile()
-    //     return content 
-    // }
-    
-    // async deleteById(){
-    //     const contenido = await this.readFile()
-    // }
-    
-    // async deleteAll(){
-        
-    // }
 }
 
+let newContent = [];
 
+const item = {
+    title: "headphone",
+    price: "15.00",
+    thumbnail: "https://www.example.com/images/keyboard.jpg"
+}
 
 const productos = new Contenedor("./productos.txt");
-const obj = {
-    title:"Calculator",
-    price:"1.00"
-}
-const obj2 = {
-    title:"mouse",
-    price:"5.00"
-}
-const obj3 = {
-    title:"keyboard",
-    price:"7.00"
-}
 
-productos.save(obj)
-productos.save(obj2)
-productos.save(obj3)
+
+productos.save(item)
+
+setTimeout(() => {
+    productos.getAll()
+    productos.getById(2)
+    productos.deleteById(1)
+    setTimeout(() => {
+        productos.deleteAll()
+    }, 500)
+}, 1000)
